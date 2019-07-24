0393ab0346a382c4c105f669171cc96ebe78c895
cybqzwork <cybqzwork@163.com>
2019-07-24 22:38:24 +0800

    设置bat方式初始化log4j2

--- IPPortTools/src/cyb/cipporttools/main/Start.java
+++ IPPortTools/src/cyb/cipporttools/main/Start.java
@@ -3,6 +3,16 @@
 */
 package cyb.cipporttools.main;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+
 import cyb.cipporttools.exec.proxy.DynamicProxyHandler;
 import cyb.cipporttools.exec.proxy.ProxyStatic;
 import cyb.cipporttools.exec.service.ExecService;
@@ -23,9 +33,36 @@ import cyb.cipporttools.exec.service.impl.ExecServiceImpl;
 public class Start {
 
 	public static void main(String[] args) {
-		/*ExecService proxy = new ProxyStatic();
-		proxy.ping("www.baidu.com");*/
-		ExecService dynamicProxy = (ExecService) new DynamicProxyHandler().getInstance(new ExecServiceImpl());
-		dynamicProxy.ping("www.baidu.com");
+		if(initLogger(args)) {
+			/*ExecService proxy = new ProxyStatic();
+			proxy.ping("www.baidu.com");*/
+			ExecService dynamicProxy = (ExecService) new DynamicProxyHandler().getInstance(new ExecServiceImpl());
+			dynamicProxy.ping("www.baidu.com");
+		}
+	}
+
+	private static boolean initLogger(String[] args) {
+		try {
+			if(args != null && args.length > 0 && args[0].equals("log4j2")) {
+				String logPath = Start.class.getResource("/").toString() + "log4j2.xml";
+				System.out.println(logPath);
+				File file = new File(logPath);
+				if(file.exists()) {
+					try {
+						ConfigurationSource source = new ConfigurationSource(new FileInputStream(file));
+						Configurator.initialize(null, source);
+						return true;
+					} catch (FileNotFoundException e1) {
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
